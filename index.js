7d23c66704c3f5e359e6afbf7c3b31b0b034870c
Haroon Khan <hkhan6916@gmail.com>
2022-04-09 18:26:11 +0100

    test

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const io = new Server(server, {
 //   },
 // });
 
-const redisUrl = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+const redisUrl = `rediss://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
 
 const pubClient = createClient({ url: redisUrl, password: process.env.REDIS_KEY });
 const subClient = pubClient.duplicate();
