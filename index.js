8ffe9a69f23a837e31a799c3867b16c3d1c2f8bd
Haroon Khan <hkhan6916@gmail.com>
2022-04-10 18:49:44 +0100

    test

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
 // const socketIo = require('socket.io');
-// const { Server } = require('socket.io');
-// const { createClient } = require('redis');
-// const { createAdapter } = require('@socket.io/redis-adapter');
+const { Server } = require('socket.io');
+const { createClient } = require('redis');
+const { createAdapter } = require('@socket.io/redis-adapter');
 // const stickySession = require('sticky-session');
-// const user = require('./src/routes/User');
-// const posts = require('./src/routes/Posts');
-// const jobs = require('./src/routes/Jobs');
-// const file = require('./src/routes/File');
-// const chat = require('./src/routes/Chat');
-// const notifications = require('./src/routes/Notifications');
+const user = require('./src/routes/User');
+const posts = require('./src/routes/Posts');
+const jobs = require('./src/routes/Jobs');
+const file = require('./src/routes/File');
+const chat = require('./src/routes/Chat');
+const notifications = require('./src/routes/Notifications');
 
 const port = process.env.PORT || 5000;
 
@@ -24,23 +24,23 @@ mongoose.connect(process.env.DB_CONNECT, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-// const io = new Server(server, {
-//   cors: {
-//     origin: '*',
-//     credentials: true, // for sticky sessions to work so cookies can be sent
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    credentials: true, // for sticky sessions to work so cookies can be sent
+  },
+});
 
-// const redisUrl = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+const redisUrl = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
 
-// const pubClient = createClient({ url: redisUrl, password: process.env.REDIS_KEY });
-// const subClient = pubClient.duplicate();
+const pubClient = createClient({ url: redisUrl, password: process.env.REDIS_KEY });
+const subClient = pubClient.duplicate();
 
-// Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
-//   io.adapter(createAdapter(pubClient, subClient));
-// });
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+  io.adapter(createAdapter(pubClient, subClient));
+});
 
-// require('./src/routes/Messages.socket')(io);
+require('./src/routes/Messages.socket')(io);
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -48,6 +48,6 @@ app.use(express.json());
 
 app.use(cors({ origin: '*' }));
 
-// app.use(user, posts, jobs, file, chat, notifications);
+app.use(user, posts, jobs, file, chat, notifications);
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
