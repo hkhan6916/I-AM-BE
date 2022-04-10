c112bc6ccd8c6f5e6a3fa481a5d890c947fa64a8
Haroon Khan <hkhan6916@gmail.com>
2022-04-10 18:52:15 +0100

    test

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
-// const socketIo = require('socket.io');
-const { Server } = require('socket.io');
-const { createClient } = require('redis');
-const { createAdapter } = require('@socket.io/redis-adapter');
-// const stickySession = require('sticky-session');
+const socketIo = require('socket.io');
 const user = require('./src/routes/User');
 const posts = require('./src/routes/Posts');
 const jobs = require('./src/routes/Jobs');
@@ -19,31 +15,18 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-
 mongoose.connect(process.env.DB_CONNECT, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const io = new Server(server, {
+const io = socketIo(server, {
   cors: {
     origin: '*',
-    credentials: true, // for sticky sessions to work so cookies can be sent
   },
 });
-
-const redisUrl = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
-
-const pubClient = createClient({ url: redisUrl, password: process.env.REDIS_KEY });
-const subClient = pubClient.duplicate();
-
-Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
-  io.adapter(createAdapter(pubClient, subClient));
-});
-
 require('./src/routes/Messages.socket')(io);
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 
 app.use(cors({ origin: '*' }));
