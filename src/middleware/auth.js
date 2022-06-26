127549acfb06287b4da03551c03d3a4a29cc3cba
Haroon Khan <hkhan6916@gmail.com>
2022-06-26 20:34:13 +0100

    fix cannot send headers after... error in auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,13 +1,13 @@
 const JWT = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  if (!req.headers.authorization) throw new Error('Unauthorised');
+  if (!req.headers.authorization) { res.status(200).json({ message: 'Unauthorised' }); return; }
   const authHeader = req.headers.authorization;
   const bearerToken = authHeader?.split(' ');
   const token = bearerToken?.[1];
   JWT.verify(token, process.env.TOKEN_SECRET, (err, user) => {
     if (err) {
-      throw new Error('Unauthorised');
+      res.status(200).json({ message: 'Unauthorised' }); return;
     }
     req.user = user;
     next();
