8e012a1db5c2aef0ede57c8075de5609b52b9146
Haroon Khan <hkhan6916@gmail.com>
2022-07-22 17:45:32 +0100

    regular commit

--- src/services/Posts/Likes.js
+++ src/services/Posts/Likes.js
@@ -5,48 +5,52 @@ const PostLikes = require('../../models/user/PostLikes');
 ########## => Like System
 */
 const addLikeToPost = async (postId, userId) => {
-  const post = await Posts.findById(postId);
+  setTimeout(async () => {
+    const post = await Posts.findById(postId);
 
-  if (!post) {
-    throw new Error('Post does not exist.');
-  }
+    if (!post) {
+      throw new Error('Post does not exist.');
+    }
 
-  if (post.userId.toString() === userId) {
-    throw new Error('Cannot like this post as it belongs to the same user.');
-  }
+    if (post.userId.toString() === userId) {
+      throw new Error('Cannot like this post as it belongs to the same user.');
+    }
 
-  const likedPost = await PostLikes.findOne({ likedBy: userId, postId });
-  if (likedPost) {
-    throw new Error('User has already liked this post.');
-  }
+    const likedPost = await PostLikes.findOne({ likedBy: userId, postId });
+    if (likedPost) {
+      throw new Error('User has already liked this post.');
+    }
 
-  const like = new PostLikes({
-    likedBy: userId,
-    postId,
-  });
+    const like = new PostLikes({
+      likedBy: userId,
+      postId,
+    });
 
-  post.likes += 1;
-  post.save();
-  like.save();
+    post.likes += 1;
+    post.save();
+    like.save();
 
-  return 'Post has been liked';
+    return 'Post has been liked';
+  }, 500);
 };
 
 const removeLikeFromPost = async (postId, userId) => {
-  const post = await Posts.findById(postId);
-  if (!post) {
-    throw new Error('Post does not exist.');
-  }
-  const likedPost = await PostLikes.findOneAndDelete({ likedBy: userId, postId });
-  if (!likedPost) {
-    throw new Error('User has not liked this post yet.');
-  }
+  setTimeout(async () => {
+    const post = await Posts.findById(postId);
+    if (!post) {
+      throw new Error('Post does not exist.');
+    }
+    const likedPost = await PostLikes.findOneAndDelete({ likedBy: userId, postId });
+    if (!likedPost) {
+      throw new Error('User has not liked this post yet.');
+    }
 
-  if (post.likes > 0) {
-    post.likes -= 1;
-    post.save();
-  }
-  return 'Post like has been removed';
+    if (post.likes > 0) {
+      post.likes -= 1;
+      post.save();
+    }
+    return 'Post like has been removed';
+  }, 500);
 };
 
 module.exports = {
